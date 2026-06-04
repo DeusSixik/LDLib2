f5fc6bbafe9d8d4c7a502d4bbe9bd90e53711c72
Bogdan <142807122+DeusSixik@users.noreply.github.com>
2026-06-04 22:32:04 +0300

    Update TestJava.java

--- src/main/java/com/lowdragmc/lowdraglib2/test/TestJava.java
+++ src/main/java/com/lowdragmc/lowdraglib2/test/TestJava.java
@@ -27,8 +27,18 @@ public class TestJava {
         );
 
         Map<String, Collection<ProfileData.Snapshot>> comparison = new LinkedHashMap<>();
-        comparison.put("LDLib", Profiler.load("E:\\JavaProjects\\LDLib2\\runs\\client\\run_v2.dump"));
-        comparison.put("LDLib Path", Profiler.load("E:\\JavaProjects\\LDLib2\\runs\\client\\run_v1.dump"));
+
+        List<ProfileData.Snapshot> no_path = new ArrayList<>();
+        no_path.addAll(Profiler.load("E:\\JavaProjects\\LDLib2\\runs\\client\\run_v2.dump"));
+        no_path.addAll( Profiler.load("E:\\JavaProjects\\LDLib2\\runs\\client\\graph_no_path.dump"));
+
+        List<ProfileData.Snapshot> path = new ArrayList<>();
+        path.addAll(Profiler.load("E:\\JavaProjects\\LDLib2\\runs\\client\\run_v1.dump"));
+        path.addAll( Profiler.load("E:\\JavaProjects\\LDLib2\\runs\\client\\graph_path.dump"));
+
+        comparison.put("LDLib", no_path);
+        comparison.put("LDLib Path", path);
+//        comparison.put("LDLib_ Path", Profiler.load("E:\\JavaProjects\\LDLib2\\runs\\client\\graph_path.dump"));
 
         HtmlReporter.generateComparison("comparison_report.html", comparison, specs);
     }
